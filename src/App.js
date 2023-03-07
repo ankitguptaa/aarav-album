72ffd354430c2f0454d073c5496c777b26543e1c
Ankit Gupta <ankitguptaa@hotmail.com>
2023-03-07 09:39:31 +0530

    Upload form code added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Title from './comps/Title';
-
+import UploadForm from './comps/uploadform';
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm></UploadForm>
     </div>
   );
 }
